266cf4b6a1a32097cd4187a174bfdf0235a585cf
llutze2s <32838560+llutze2s@users.noreply.github.com>
2019-05-06 18:23:37 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,12 @@
+#!/bin/bash
+
 #npm starten auf der CLI
-CMD [ "npm", "start" ]
-CMD [ "npm", "i" , "--save-dev" , "supertest" , "should" , "mocha"]
-CMD [ "npm", "i" , "-g" , "mocha"]
+npm start
+npm i --save-dev supertest should mocha
+npm i -g mocha
 
 #index.js ausführen
-CMD ["node","index.js"]
+node index.js
 
-mocha
+#neues Terminal öffnen
+term -e "mocha"
