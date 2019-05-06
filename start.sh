9667eba3389895934a99cb58e839b414ef4a3344
llutze2s <32838560+llutze2s@users.noreply.github.com>
2019-05-06 18:18:07 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+#npm starten auf der CLI
+CMD [ "npm", "start" ]
+CMD [ "npm", "i" , "--save-dev" , "supertest" , "should" , "mocha"]
+CMD [ "npm", "i" , "-g" , "mocha"]
+
+#index.js ausführen
+CMD ["node","index.js"]
+
+mocha
